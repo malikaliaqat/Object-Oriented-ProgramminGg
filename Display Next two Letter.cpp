72c149587a249f2dca0c429a8d8965238c7b1cc6
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-09 22:11:12 +0500

    Add files via upload

---
+++ Display Next two Letter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter a character is:";
+	cin>>ch;
+	cout<<"Next two character is:\t";
+	ch++;
+	cout<<"first is"<<ch;
+    ch++;
+    cout<<"\tsecond  is\t"<<ch;
+}
