ef7e48c458b26376f185f7ad46382b476663d699
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-09 22:09:07 +0500

    Add files via upload

---
+++ Array Example#1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5],i;
+	cout<<"Enter five integers:"<<endl;
+	cin>>arr[0];
+	cin>>arr[1];
+		cin>>arr[2];
+			cin>>arr[3];
+				cin>>arr[4];
+				cout<<"The value in arry are:\n";
+				cout<<arr[0]<<endl;
+				cout<<arr[1]<<endl;
+				cout<<arr[2]<<endl;
+				cout<<arr[3]<<endl;
+				cout<<arr[4]<<endl;	
+				}
