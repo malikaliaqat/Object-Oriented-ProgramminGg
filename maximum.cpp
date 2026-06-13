098198548fb5caf8b22f99413881a6b9245c0d98
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-13 21:11:34 +0500

    Add files via upload

---
+++ maximum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int max,i,arr[5];
+	for(i=0;i<5;i++)
+	{
+		cout<<"Enetr a num";
+		cin>>arr[i];
+	}
+	max=arr[0];
+	if(arr[i]>max)
+	{
+	max=arr[i];
+	cout<<"Maximun num is"<<max;
+}
+}
