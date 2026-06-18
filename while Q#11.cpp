0362bbfc3d7331093c39d28f7c2caceb0d57f2f1
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-18 16:19:39 +0500

    Add files via upload

---
+++ while Q#11.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main()
+{
+		int n,m,h,h2;
+			cout<<" Eneter Number of student";
+	        cin>>n;
+		int p=2;
+		cout<<"Enter Marks";
+		cin>>m;
+		while(p<=n)
+		{
+			p++;
+		if(m>h2 && m>h)
+		{
+		h=m;
+	   }
+	if(m>h)
+	{
+		h2=m;
+	}
+	}
+		cout<<"Highest Marks are;"<<h<<endl;
+				cout<<"Second highest Marks are:"<<h2;
+				return 0;
+}
