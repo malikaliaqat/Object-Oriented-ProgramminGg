d336b28b86a007706a517e094de00dc0e78d8eeb
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-21 16:48:46 +0500

    Add files via upload

---
+++ staric.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int k,h;
+	cin>>k>>h;
+	for(int khadi=1;khadi<=k;khadi++)
+		for(int horia=1;horia<h;horia++)
+	{
+	cout<<"*"<<endl;
+    }
+}
+
+
