570a682be3274c922318c43c724570424f627b6b
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-06-21 16:48:27 +0500

    Add files via upload

---
+++ Student marks Q#11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n,m,h,h2;
+	cout<<" Enter Number of student";
+	cin>>n;
+		cout<<"Enter Marks";
+		cin>>m;
+		if(m>h)
+		{
+		    h2=m;
+	    }
+	    else
+	    {
+	    	h=m;
+		}
+			cout<<"Highest Marks are;"<<h<<endl;
+				cout<<"Second highest Marks are:"<<h2;
+				return 0;
+}  
